61b753fa984c8c018e9f445f37ac599f87eb3482
keavyknauss <44242165+keavyknauss@users.noreply.github.com>
2018-11-02 09:43:37 -0500

    init commit

---
+++ App/Class Project/Server/config/config.js
@@ -0,0 +1,25 @@
+var path = require('path'),
+    rootPath = path.normalize(__dirname + '/..'),
+    env = process.env.NODE_ENV || 'development';
+var config = {
+    development: {
+        root: rootPath,
+        app: { name: 'UCCSS' },
+        port: 3300,
+        db: 'mongodb://127.0.0.1/helpMe-dev'
+    },
+    test: {
+        root: rootPath,
+        app: {  name: 'UCCSS'   },
+        port: 3300,
+        db: 'mongodb://127.0.0.1/helpMe-test'
+    },
+
+    production: {
+        root: rootPath,
+        app: { name: 'UCCSS' },
+        port: 3300,
+        db: 'mongodb://127.0.0.1/helpMe'
+    }
+};
+module.exports = config[env];
